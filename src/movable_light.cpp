6e68d3de8bdfcde83cc76092d2713659b1d28037
David Sveningsson <ext@sidvind.com>
2012-09-07 15:32:29 +0200

    cosmetic: reindent.

--- src/movable_light.cpp
+++ src/movable_light.cpp
@@ -4,15 +4,15 @@
 
 #include "movable_light.hpp"
 
-MovableLight::MovableLight(Light * light) :
-		MovableObject(light->position)
+MovableLight::MovableLight(Light * light)
+	: MovableObject(light->position)
 	, constant_attenuation(light->constant_attenuation)
 	, linear_attenuation(light->linear_attenuation)
 	, quadratic_attenuation(light->quadratic_attenuation)
 	, type(light->type)
 	, intensity(light->intensity)
 	, data(light)
-	{ }
+{}
 
 void MovableLight::update() {
 	data->position = position_;
