2bb6ce058c35c81d6ee085b0d65203121056bf46
Andreas Tarandi <torandi@gmail.com>
2012-09-07 15:30:40 +0200

    Removing usage of swizzle for better compability with MSVC.

--- src/movable_light.cpp
+++ src/movable_light.cpp
@@ -2,13 +2,10 @@
 #include "config.h"
 #endif
 
-#define GLM_SWIZZLE
-
 #include "movable_light.hpp"
-#include <glm/glm.hpp>
 
 MovableLight::MovableLight(Light * light) :
-		MovableObject(light->position.xyz)
+		MovableObject(light->position)
 	, constant_attenuation(light->constant_attenuation)
 	, linear_attenuation(light->linear_attenuation)
 	, quadratic_attenuation(light->quadratic_attenuation)
