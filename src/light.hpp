560b82d8abd49840f5e12831b8cb36818e6e5767
Andreas Tarandi <torandi@gmail.com>
2012-09-10 22:08:45 +0200

    Implement shadow maps for directional lights
    Conflicts:
    	src/light.hpp

--- src/light.hpp
+++ src/light.hpp
@@ -14,7 +14,9 @@ struct Light {
 	glm::vec3 __ALIGNED__(16) intensity;
 	glm::vec3 __ALIGNED__(16) position;
 	glm::mat4 __ALIGNED__(16) matrix;
-	GLint __ALIGNED__(16) shadowmap_index;
+	glm::vec2 __ALIGNED__(16) shadowmap_scale; // 1/width, 1/height of shadowmap resolution
+	GLint shadowmap_index;
+	float shadow_bias;
 };
 
 #endif
