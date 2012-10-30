a864bf08fc2b566b8a7539ead9a8f76141f6a12a
David Sveningsson <ext@sidvind.com>
2012-10-30 12:26:05 +0100

    add shadowmap

--- src/nx15.cpp
+++ src/nx15.cpp
@@ -5,6 +5,7 @@
 #include "data.hpp"
 #include "engine.hpp"
 #include "lights_data.hpp"
+#include "render_object.hpp"
 #include "rendertarget.hpp"
 #include "terrain.hpp"
 #include "time.hpp"
@@ -14,6 +15,7 @@ static Shader* passthru = nullptr;
 static TextureArray* colormap = nullptr;
 static TextureArray* normalmap = nullptr;
 static Terrain* terrain = nullptr;
+static RenderObject* obj = nullptr;
 static RenderTarget* scene = nullptr;
 static LightsData* lights = nullptr;
 static Camera cam(75, 1.3f, 0.1f, 100.0f);
@@ -34,11 +36,12 @@ namespace Engine {
 		terrain   = new Terrain("/nx15/terrain.png", 15.0f, 4.0f, colormap, normalmap);
 		scene     = new RenderTarget(resolution, GL_RGB8, RenderTarget::DEPTH_BUFFER, GL_LINEAR);
 		lights    = new LightsData();
+		obj       = new RenderObject("/models/bench.obj", true);
 
 		terrain->set_position(glm::vec3(-7.5f, -2.0f, -7.5f));
 		lights->ambient_intensity() = glm::vec3(0.1f);
 		lights->num_lights() = 1;
-		lights->lights[0]->set_position(glm::vec3(0, -0.5f, -1.f));
+		lights->lights[0]->set_position(glm::vec3(0, -0.3f, -1.f));
 		lights->lights[0]->intensity = glm::vec3(0.8f);
 		lights->lights[0]->type = MovableLight::DIRECTIONAL_LIGHT;
 	}
@@ -52,18 +55,42 @@ namespace Engine {
 		Shader::cleanup();
 	}
 
-	void render(){
+	static void render_geometry(){
+		RenderTarget::clear(Color::magenta);
+		terrain->render();
+	}
+
+	static void render_scene(){
+		lights->lights[0]->render_shadow_map(cam, [](const glm::mat4 &m) -> void  {
+			render_geometry();
+		});
+
 		Shader::upload_projection_view_matrices(cam.projection_matrix(), cam.view_matrix());
 		Shader::upload_model_matrix(glm::mat4());
 		Shader::upload_lights(*lights);
 		Shader::upload_blank_material();
 		Shader::upload_resolution(resolution);
 		RenderTarget::clear(Color::blue);
-		terrain->render();
+
+		scene->with([](){
+			render_geometry();
+		});
+	}
+
+	static void render_blit(){
+		Shader::upload_projection_view_matrices(screen_ortho, glm::mat4());
+		Shader::upload_model_matrix(glm::mat4());
+		RenderTarget::clear(Color::magenta);
+		scene->draw(passthru, glm::vec2(0,0), glm::vec2(resolution));
+	}
+
+	void render(){
+		render_scene();
+		render_blit();
 	}
 
 	void update(float t, float dt){
-		const float s = t*0.5f;
+		const float s = t*0.2f;
 		const float d = 7.5f;
 
 		cam.look_at(glm::vec3(0,0,0));
