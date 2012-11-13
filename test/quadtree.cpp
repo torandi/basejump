76c6dbe03e21f2029fd71cf5cbb4143855a32152
Andreas Tarandi <torandi@gmail.com>
2012-11-13 20:13:56 +0100

    More quadtree tests

--- test/quadtree.cpp
+++ test/quadtree.cpp
@@ -19,6 +19,8 @@ class Test: public CppUnit::TestFixture {
   CPPUNIT_TEST_SUITE(Test);
 	CPPUNIT_TEST(test_create);
 	CPPUNIT_TEST(test_traverse);
+	CPPUNIT_TEST(test_create_inverse);
+	//CPPUNIT_TEST(test_traverse_inverse);
   CPPUNIT_TEST_SUITE_END();
 
 public:
@@ -53,6 +55,28 @@ public:
 		return tree;
 	}
 
+	QuadTree * create_bottom_up_tree(int size) {
+		int side = static_cast<int>(glm::exp2(static_cast<float>(size)));
+		QuadTree * tree = new QuadTree(AABB_2D(glm::vec2(0.f), glm::vec2(1.f)), 0);
+
+		sum = 0;
+
+		for(int y=0; y<side; ++y) {
+			for(int x=0; x<side; ++x) {
+				test_data * d = new test_data();
+				d->index = (y * side + x);
+				sum += d->index;
+				QuadTree * node = nullptr;
+				while(node == nullptr) {
+					node = tree->child(glm::vec2(static_cast<float>(x), static_cast<float>(y)));
+					if(node == nullptr) tree = tree->grow();
+				}
+				node->data = (void*) d;
+			}
+		}
+		return tree;
+	}
+
 	void debug_traverse(QuadTree * tree) {
 		printf("Tree debug print\n");
 		tree->traverse([](QuadTree * node) -> bool {
@@ -79,6 +103,29 @@ public:
 	void test_traverse() {
 		static const int size = 5;
 		QuadTree * tree = create_top_down_tree(size);
+		int accum = 0;
+		tree->traverse([&accum](QuadTree * tree) -> bool {
+			if(tree->level() == 0) accum += ((test_data*) tree->data)->index;
+			return true;
+		});
+
+		CPPUNIT_ASSERT_EQUAL(sum, accum);
+		free_tree(tree);
+	}
+
+	void test_create_inverse() {
+		static const int size = 3;
+		QuadTree * tree = create_bottom_up_tree(size);
+		CPPUNIT_ASSERT_EQUAL(size, tree->level());
+		QuadTree * node = tree->child(glm::vec2(0.f));
+		CPPUNIT_ASSERT(node != nullptr);
+		CPPUNIT_ASSERT_EQUAL(0, node->level());
+		free_tree(tree);
+	}
+
+	void test_traverse_inverse() {
+		static const int size = 5;
+		QuadTree * tree = create_bottom_up_tree(size);
 		int accum = 0;
 		tree->traverse([&accum](QuadTree * tree) -> bool {
 			if(tree->level() == 0) accum += ((test_data*) tree->data)->index;
