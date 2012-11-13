eced3d41f6209855881a697cb9946ce362d217b3
Andreas Tarandi <torandi@gmail.com>
2012-11-13 19:56:42 +0100

    ADd vec2 asserts

--- test/asserts.hpp
+++ test/asserts.hpp
@@ -6,6 +6,17 @@
 #include <glm/glm.hpp>
 #include <cstring>
 
+static void check_vec2(const glm::vec2 &expected, const glm::vec3 &actual, float delta, const char * message, CppUnit::SourceLine sourceline) {
+	char * buffer = (char*) malloc(strlen(message) + 3);
+
+	sprintf(buffer, "%s.x", message);
+	CppUnit::assertDoubleEquals((double)expected.x,(double) actual.x, (double)delta, sourceline, buffer);
+	sprintf(buffer, "%s.y", message);
+	CppUnit::assertDoubleEquals((double)expected.y,(double) actual.y, (double)delta,sourceline,  buffer);
+
+	free(buffer);
+}
+
 static void check_vec3(const glm::vec3 &expected, const glm::vec3 &actual, float delta, const char * message, CppUnit::SourceLine sourceline) {
 	char * buffer = (char*) malloc(strlen(message) + 3);
 
@@ -18,6 +29,9 @@ static void check_vec3(const glm::vec3 &expected, const glm::vec3 &actual, float
 
 	free(buffer);
 }
+
+#define CPPUNIT_ASSERT_VEC2_EQUAL(expected, actual, delta) check_vec2(expected, actual, delta,"", CPPUNIT_SOURCELINE())
+#define CPPUNIT_ASSERT_VEC2_EQUAL_MESSAGE(message, expected, actual, delta) check_vec2(expected, actual, delta, message, CPPUNIT_SOURCELINE())
 
 #define CPPUNIT_ASSERT_VEC3_EQUAL(expected, actual, delta) check_vec3(expected, actual, delta,"", CPPUNIT_SOURCELINE())
 #define CPPUNIT_ASSERT_VEC3_EQUAL_MESSAGE(message, expected, actual, delta) check_vec3(expected, actual, delta, message, CPPUNIT_SOURCELINE())
