c5259499b7952bebad67150a8d1fabdf0b73f39f
Andreas Tarandi <torandi@gmail.com>
2012-11-13 15:38:32 +0100

    Add "quadtree" that is not actually a quadtree

---
+++ src/quadtree.cpp
@@ -0,0 +1,85 @@
+#include "quadtree.hpp"
+
+QuadTree::QuadTree(AABB_2D position, int level)
+	: aabb(position)
+	, data(nullptr)
+	, level_(level)
+{
+		for(int i=0; i<4; ++i) children[i] = nullptr;
+}
+
+QuadTree::~QuadTree() {
+	for(int i=0; i<4; ++i) {
+		if(children[i] != nullptr) delete children[i];
+	}
+}
+
+void QuadTree::add_child(QuadTree * node) {
+	if(aabb.contains(node->aabb.min) && aabb.contains(node->aabb.max)) {
+		int index = child_index(node->aabb.middle());
+		if(children[index] != nullptr) delete children[index]; //Dellocated anything in that slot
+		children[index] = node;
+	} else {
+		Logging::fatal("[QuadTree] Trying to add a child that is larger than the parent\n");
+	}
+}
+
+QuadTree * QuadTree::child(const glm::vec2 &position) {
+	if(aabb.contains(position)) {
+		if(level_ == 0) return this; //This is a leaf
+
+		int index = child_index(position);
+		return child(index);
+	} else {
+		return nullptr;
+	}
+}
+
+QuadTree * QuadTree::child(int index) {
+	if(children[index] == nullptr) create_child(index);
+	return children[index];
+}
+
+int QuadTree::child_index(const glm::vec2 &position) {
+	if(position.x < aabb.middle().x) {
+		if(position.y < aabb.middle().y) {
+			return 0;
+		} else {
+			return 2;
+		}
+	} else {
+		if(position.y < aabb.middle().y) {
+			return 1;
+		} else {
+			return 3;
+		}
+	}
+}
+
+int QuadTree::level() const {
+	return level_;
+}
+
+void QuadTree::create_child(int index) {
+	glm::vec2 half_size = aabb.size() / 2.f;
+	glm::vec2 min = aabb.min;
+	switch(index) {
+		case 3:
+			min.x += half_size.x;
+		case 2:
+			min.y += half_size.y;
+			break;
+		case 1:
+			min.x += half_size.x;
+			break;
+	}
+	children[index] = new QuadTree(AABB_2D(min, min+half_size), level_ - 1);
+}
+
+void QuadTree::traverse(const std::function<bool(QuadTree*)> & func) {
+	if(func(this)) {
+		for(int i=0; i<4; ++i) {
+			if(children[i] != nullptr) children[i]->traverse(func);
+		}
+	}
+}
---
+++ src/quadtree.hpp
@@ -0,0 +1,73 @@
+#ifndef QUADTREE_HPP
+
+#include <functional>
+#include <glm/glm.hpp>
+
+#include "aabb2d.hpp"
+
+/*
+ * This is actually just a node in a quad tree, but
+ * the first node is the root of the tree
+ *
+ * NOTE: Apparently this is not a real quad tree (more like a 2d-tree), but it works for the current purpose
+ */
+
+class QuadTree {
+	public:
+		QuadTree(AABB_2D position, int level = 0);
+		~QuadTree();
+
+		void * data;
+
+		/*
+		 * Traverse the tree, executing given function at each node.
+		 * Return true from function to continue down the branch
+		 */
+		void traverse(const std::function<bool(QuadTree*)> & func);
+
+		/*
+		 * Finds or allocates child that contain the given position, 
+		 * if the position is in this quad tree a quadtree ptr will be returned
+		 * if the position is not in this quad tree, a nullptr will be returned
+		 */
+		QuadTree * child(const glm::vec2 position);
+
+		/*
+		 * Same as above, but from specified index
+		 */
+		QuadTree * child(int index);
+
+		/*
+		 * Add the node to this quad tree. 
+		 * Will abort (log fatal) if the aabb is not contain within this nodes aabb,
+		 * but no other sanity check will be made (such as that it is exactly 1/4 of this aabb),
+		 * but if that is not the case, things will be weird.
+		 *
+		 * If the slot that the child should occupy is already taken the old child will be deallocaded.
+		 */
+		void add_child(QuadTree * node);
+
+		const AABB_2D aabb;
+
+		int level() const;
+	private:
+		/**
+		 * 0 | 1
+		 * -----
+		 * 2 | 3
+		 */
+		QuadTree * children[4]; 
+
+		int level_; //Level 0 is leaf, all above are non-teriminal nodes
+
+		/**
+		 * Calculates which quadrant the position lies in.
+		 * Assumes that the position is in this nodes aabb
+		 */
+		int child_index(const glm::vec2 position);
+
+		void create_child(int index);
+
+};
+
+#endif
