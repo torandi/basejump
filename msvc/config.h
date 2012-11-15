291f04d5e7ab5c9ee52f8cac13cbfeaa9e9eeecf
Andreas Tarandi <torandi@gmail.com>
2012-11-15 04:29:17 +0100

    msvc config.h cleanup

--- msvc/config.h
+++ msvc/config.h
@@ -1,16 +1,11 @@
 /* package settings */
-#define PACKAGE "frobnicator-demo-engine"
-#define PACKAGE_BUGREPORT "ext@sidvind.com"
-#define PACKAGE_NAME "frobnicator-demo-engine"
-#define PACKAGE_STRING "frobnicator-demo-engine 1"
-#define PACKAGE_TARNAME "frobnicator-demo-engine"
-#define PACKAGE_URL ""
-#define PACKAGE_VERSION "1"
-#define VERSION "1"
+#define PACKAGE "basejump"
+#define PACKAGE_BUGREPORT "taran@kth.se"
+#define PACKAGE_NAME "basejump"
+#define VERSION "0.1"
 
 /* frob-specific settings */
 #define FULLSCREEN 0
-#define NOLOAD 1
 #define srcdir ""
 #define datadir ""
 
